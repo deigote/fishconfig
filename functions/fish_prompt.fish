492c83a695e3e91ae62b33dbbbb2474280db140d
deigote <diego@toharia.com>
2014-04-29 15:28:40 +0200

    When available, the command duration is displayed, and when it was long, a notification is sentl

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,13 +3,20 @@ function _is_git_dirty
 end
 
 function fish_prompt
-  set_color green
-  echo -n (basename (pwd | sed "s,^$HOME,~,"))
-  set_color cyan
-  echo -n (__git_ps1)
-  if [ (_is_git_dirty) ]
-    echo -n " *"
-  end  
-  echo -n ' $ '
-  set_color normal
+   if set -q CMD_DURATION
+      echo \a(set_color 555555)\> Took $CMD_DURATION
+      set -l time_sec (echo $CMD_DURATION | cut -d'.' -f1)
+      if test $time_sec -gt 10 ;and which terminal-notifier > /dev/null
+         terminal-notifier -message "Shell command (commandline) is done!" -title "Command done"
+      end
+   end
+   set_color green
+   echo -n (basename (pwd | sed "s,^$HOME,~,"))
+   set_color cyan
+   echo -n (__git_ps1)
+   if [ (_is_git_dirty) ]
+      echo -n " *"
+   end  
+   echo -n ' $ '
+   set_color normal
 end
