5aac04ae9bbad0266a58c8ce6e6cc27e431b1445
deigote <diego@toharia.com>
2014-04-29 15:28:55 +0200

    Some classic aliases

--- config.fish
+++ config.fish
@@ -1,2 +1,7 @@
 set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
+
+alias ps='ps aux -o user,uid,pid,ppid,nice,%cpu,%mem,rss,vsize,etime,args'
+alias psg='ps | head -n 1 ; and ps | grep -i'
+alias hig='history | grep -i'
+
