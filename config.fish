35cfd02346ed039277693c0a7f520f0f65bf4e32
deigote <diego@toharia.com>
2014-05-14 17:33:46 +0200

    Some java related environment variables

--- config.fish
+++ config.fish
@@ -1,7 +1,15 @@
 set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
-set PATH $PATH /usr/local/mysql/bin/
+set PATH $PATH $HOME/local/mysql/bin/ $HOME/local/mysql/support-files/
 
-alias ps='ps aux -o user,uid,pid,ppid,nice,%cpu,%mem,rss,vsize,etime,args'
+# Java and friends
+set JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF8 -Xmx4096m -Xms256m -XX:PermSize=256M -XX:MaxPermSize=4096M"
+set JAVA_OPTS $JAVA_TOOL_OPTIONS
+set GRAILS_OPTS $JAVA_TOOL_OPTIONS
+
+# Android SDK
+set ANDROID_HOME /usr/local/opt/android-sdk
+
+alias ps='ps auxw -o user,uid,pid,ppid,nice,%cpu,%mem,rss,vsize,etime,args'
 alias psg='ps | head -n 1 ; and ps | grep -i'
 alias hig='history | grep -i'
-
+alias grep='grep -i --color'
