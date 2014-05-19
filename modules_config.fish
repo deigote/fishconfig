60ebb10b7acbfd3fea04966472d28dca1222c335
deigote <diego@toharia.com>
2014-05-19 22:30:53 +0200

    Moved mysql functions to module

--- modules_config.fish
+++ modules_config.fish
@@ -1,2 +1,3 @@
 module "git@github.com:deigote/fish-common.git" fish-common
 module "git@github.com:deigote/gh-issues-flow.git" gh-issues-flow
+module "git@github.com:deigote/fish-mysql-utils.git" mysql-utils
