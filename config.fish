c30a9db39efc3aa066a20c369f13773f38c2b074
deigote <diego@toharia.com>
2014-05-20 19:49:23 +0200

    Environment variables should be exported

--- config.fish
+++ config.fish
@@ -1,13 +1,13 @@
-set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
-set PATH $PATH $HOME/local/mysql/bin/ $HOME/local/mysql/support-files/
+set -x PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
+set -x PATH $PATH $HOME/local/mysql/bin/ $HOME/local/mysql/support-files/
 
 # Java and friends
-set JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF8 -Xmx4096m -Xms256m -XX:PermSize=256M -XX:MaxPermSize=4096M"
-set JAVA_OPTS $JAVA_TOOL_OPTIONS
-set GRAILS_OPTS $JAVA_TOOL_OPTIONS
+set -x JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF8 -Xmx4096m -Xms256m -XX:PermSize=256M -XX:MaxPermSize=4096M"
+set -x JAVA_OPTS $JAVA_TOOL_OPTIONS
+set -x GRAILS_OPTS $JAVA_TOOL_OPTIONS
 
 # Android SDK
-set ANDROID_HOME /usr/local/opt/android-sdk
+set -x ANDROID_HOME /usr/local/opt/android-sdk
 
 alias ps='ps auxw -o user,uid,pid,ppid,nice,%cpu,%mem,rss,vsize,etime,args'
 alias psg='ps | head -n 1 ; and ps | grep -i'
