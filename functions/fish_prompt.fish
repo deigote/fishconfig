fc01d2746ae2588aabbe8a492fb3706c1e45cb54
deigote <diego@toharia.com>
2015-07-13 10:44:14 +0200

    the prompt function assumes CMD_DURATION is in milliseconds instead of seconds

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,21 +4,27 @@ end
 
 function fish_prompt
    if set -q CMD_DURATION
-      echo \a(set_color 555555)\> Took $CMD_DURATION
-      set -l time_sec (echo $CMD_DURATION | cut -d'.' -f1)
-      if test $time_sec -gt 10 ;and which terminal-notifier > /dev/null
+      echo \a(set_color 555555)\> Took $CMD_DURATION ms
+      set -l time_sec (echo $CMD_DURATION)
+      if test $time_sec -gt 10000 ;and which terminal-notifier > /dev/null
          terminal-notifier -message "Shell command (commandline) is done!" -title "Command done"
       end
    end
-   set_color yellow
-   echo -n (date "+%H:%M:%S")" "
+   set git_ps (__git_ps1)
+   if test ! -z "$git_ps" 
+   	set_color cyan
+	echo -n (__git_ps1)
+        if [ (_is_git_dirty) ]
+           echo " *"
+        else
+           echo ""
+        end
+#   else
+#	echo ""
+   end
    set_color green
    echo -n (basename (pwd | sed "s,^$HOME,~,"))
-   set_color cyan
-   echo -n (__git_ps1)
-   if [ (_is_git_dirty) ]
-      echo -n " *"
-   end  
-   echo
+   set_color 555555
+   echo -n ' '\$' '
    set_color normal
 end
